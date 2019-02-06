c7254fb5e6ee1c816bba0ed43543ab3559726721
Olivia Auzenne <oauzenne@gmail.com>
2019-02-06 04:17:49 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 def oxford_comma(array)
-   case array.length 
-end 	when 1
-  "#{array[0]}"
-when 2
-  array[0..1].join(" and ")
-else 
-  array[0...-1].join(", ") << ", and #{array[-1]}"
-end 
+  return array.first if array.size == 1
+   if array.size == 2
+    array.insert(1, "and").join(" ")
+  elsif array.size > 2
+    array[-1].insert(0, "and ")
+    array.join(", ")
+  end
